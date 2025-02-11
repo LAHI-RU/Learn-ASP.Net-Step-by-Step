1b3c8d06579acb86e604178e498dc370b2bbbc83
LAHI-RU <lahiiru.dananjaya@gmail.com>
2025-02-12 01:14:31 +0530

    simple calculator

--- Learn  C#/Simple Calculator/Simple Calculator/Program.cs
+++ Learn  C#/Simple Calculator/Simple Calculator/Program.cs
@@ -1,15 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Simple_Calculator
+class Program
 {
-    internal class Program
+    static void Addition(int a, int b)
     {
-        static void Main(string[] args)
+        Console.WriteLine($"{a} + {b} = {a + b}");
+    }
+
+    static void Substraction(int a, int b)
+    {
+        Console.WriteLine($"{a} - {b} = {a - b}");
+    }
+
+    static void Multiplication(int a, int b)
+    {
+        Console.WriteLine($"{a} x {b} = {a * b}");
+    }
+
+    static void Division(int a, int b)
+    {
+        Console.WriteLine($"{a} / {b} = {a / b}");
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter the First Number: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the Operator(+,-,*,/): ");
+        char op = Convert.ToChar(Console.ReadLine());
+
+        Console.Write("Enter the Second Number: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        switch (op)
         {
+            case '+':
+                Addition(num1, num2);
+                break;
+            case '-':
+                Substraction(num1, num2);
+                break;
+            case '*':
+                Multiplication(num1, num2);
+                break;
+            case '/':
+                if (num2 != 0)
+                    Division(num1, num2);
+                else
+                    Console.WriteLine("Error! Division by zero.");
+                return;
+            default:
+                Console.WriteLine("Invalid operator!");
+                return;
         }
     }
 }
---
+++ Learn  C#/To-Do List Application/To-Do List Application/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static List<string> tasks = new List<string>();
+
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("\nTo-Do List:");
+            Console.WriteLine("1. Add Task");
+            Console.WriteLine("2. View Tasks");
+            Console.WriteLine("3. Delete Task");
+            Console.WriteLine("4. Exit");
+            Console.Write("Choose an option: ");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    AddTask();
+                    break;
+                case 2:
+                    ViewTasks();
+                    break;
+                case 3:
+                    DeleteTask();
+                    break;
+                case 4:
+                    return;
+                default:
+                    Console.WriteLine("Invalid option! Try again.");
+                    break;
+            }
+        }
+    }
+
+    static void AddTask()
+    {
+        Console.Write("Enter task: ");
+        string task = Console.ReadLine();
+        tasks.Add(task);
+        Console.WriteLine("Task added!");
+    }
+
+    static void ViewTasks()
+    {
+        Console.WriteLine("\nYour Tasks:");
+        for (int i = 0; i < tasks.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {tasks[i]}");
+        }
+    }
+
+    static void DeleteTask()
+    {
+        ViewTasks();
+        Console.Write("Enter task number to delete: ");
+        int taskNum = Convert.ToInt32(Console.ReadLine());
+
+        if (taskNum > 0 && taskNum <= tasks.Count)
+        {
+            tasks.RemoveAt(taskNum - 1);
+            Console.WriteLine("Task deleted!");
+        }
+        else
+        {
+            Console.WriteLine("Invalid task number!");
+        }
+    }
+}
