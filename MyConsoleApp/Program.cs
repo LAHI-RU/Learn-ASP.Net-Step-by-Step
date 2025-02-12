a0267a0248e2e8a1f6b8a2c33d49c382df9a5613
LAHI-RU <lahiiru.dananjaya@gmail.com>
2025-02-12 07:38:08 +0530

    update

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your name:");
+        string name = Console.ReadLine();
+        Console.WriteLine("Hello, " + name + "!");
+    }
+}
