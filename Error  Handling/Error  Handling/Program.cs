3a5630848aae49e84f4d78bfd76b38ae207ca7a6
LAHI-RU <lahiiru.dananjaya@gmail.com>
2025-02-18 06:12:02 +0530

    Error Handling

---
+++ Error  Handling/Error  Handling/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You entered: " + number);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error: Please enter a valid number.");
+        }
+        finally
+        {
+            Console.WriteLine("Program Ended.");
+        }
+    }
+}
