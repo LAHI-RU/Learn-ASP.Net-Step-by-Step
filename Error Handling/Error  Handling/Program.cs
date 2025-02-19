e6cd36b7b083c8b64379c45ecfd82072a5047b93
LAHI-RU <lahiiru.dananjaya@gmail.com>
2025-02-19 08:13:21 +0530

    error handling

--- Error Handling/Error  Handling/Program.cs
+++ Error Handling/Error  Handling/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main()
     {
+        /*
         try
         {
             Console.Write("Enter a number: ");
@@ -18,6 +19,70 @@ class Program
         {
             Console.WriteLine("Program Ended");
         }
+        */
+
+
+        /*
+        try
+        {
+            Console.Write("Enter first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int result = num1 / num2;
+            Console.WriteLine("Result: " + result);
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("⚠ Error: Cannot divide by zero!");
+        }
+        */
+
+
+        /*
+        try
+        {
+            Console.Write("Enter first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int result = num1 / num2;
+            Console.WriteLine("Result: " + result);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("⚠ Error: Please enter valid numbers.");
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("⚠ Error: Cannot divide by zero!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("⚠ Unexpected Error: " + ex.Message);
+        }
+        */
+
+
+        try
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You entered: " + number);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("⚠ Error: Please enter a valid number.");
+        }
+        finally
+        {
+            Console.WriteLine("✅ Program execution completed.");
+        }
+
     }
 
 }
---
+++ Reading and Writing Files/Reading and Writing Files/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your name:");
+        string name = Console.ReadLine();
+
+        File.WriteAllText("data.txt", "User Name: " + name);
+        Console.WriteLine("Data saved to file!");
+    }
+}
