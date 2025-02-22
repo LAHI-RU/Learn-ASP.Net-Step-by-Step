d077c5276d93d77abf8949bea70e26c66fbd40a4
LAHI-RU <lahiiru.dananjaya@gmail.com>
2025-02-22 09:16:34 +0530

    file handling complete

--- File Handling/File Handling/Program.cs
+++ File Handling/File Handling/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main()
     {
+        /*
+        // Create a File and Write Data
         string filePath = "test.txt";
 
         try
@@ -16,7 +18,82 @@ class Program
         { 
           Console.WriteLine("Error: " +  ex.Message);  
         }
+        */
 
 
+        /*
+        // Read and Display File Content 
+        string filePath = "test1.txt";
+
+        try
+        {
+            string content = File.ReadAllText(filePath);
+            Console.WriteLine("File Content: ");
+            Console.WriteLine(content);
+        }
+
+        catch(Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+
+        */
+
+
+        /*
+        // Append Data to File
+        string filePath = "test.txt";
+
+        try
+        {
+            File.AppendAllText(filePath, "\nThis is new appended text.");
+            Console.WriteLine("Text append successfully!");
+        }
+
+        catch(Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        */
+
+
+        /*
+        // Delete a File
+        string filePath = "test.txt";
+
+        try
+        {
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+                Console.WriteLine("File deleted successfully!");
+            }
+            else
+            {
+                Console.WriteLine("File not found.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        */
+
+
+        // Check File Existence
+        string filePath = "test.txt";
+
+        if (File.Exists(filePath))
+        {
+            Console.WriteLine("The file exists.");
+        }
+
+        else
+        {
+            Console.WriteLine("File does not exist.");
+        }
+
+        Console.ReadLine();
+        
     }
 }
