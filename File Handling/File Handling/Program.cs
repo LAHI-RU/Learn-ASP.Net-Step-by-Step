83297244c708d0a50bcb32d49be0be2d1aac1bbf
LAHI-RU <lahiiru.dananjaya@gmail.com>
2025-02-22 08:08:41 +0530

    error handling

---
+++ File Handling/File Handling/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+class Program
+{
+    static void Main()
+    {
+        string filePath = "test.txt";
+
+        try
+        {
+            File.WriteAllText(filePath, "Hello, this is my first file in C#!");
+            Console.WriteLine("File created and written successfully");
+        }
+
+        catch (Exception ex) 
+        { 
+          Console.WriteLine("Error: " +  ex.Message);  
+        }
+
+
+    }
+}
--- File Handling/File Handling/Properties/AssemblyInfo.cs
+++ File Handling/File Handling/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Reading and Writing Files")]
+[assembly: AssemblyTitle("File Handling")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Reading and Writing Files")]
+[assembly: AssemblyProduct("File Handling")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a964995d-8d9a-40bc-b4dc-7cb5ad717999")]
+[assembly: Guid("171f5e90-a3bb-4aaf-8db2-0bf632e55baa")]
 
 // Version information for an assembly consists of the following four values:
 //
